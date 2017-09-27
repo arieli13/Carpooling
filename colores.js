f3c22d17b4b884b1abe9fd2a9b0b6f0456f2b00b
arieli13 <arieli13_10@hotmail.com>
2017-09-27 12:56:42 -0600

    Se debe de actualizar la pantalla de login

---
+++ colores.js
@@ -0,0 +1,10 @@
+module.exports = {
+    AZUL: '#1F2F5F',
+    ROSADO: '#EF4034',
+    NARANJA: '#FFAE39',
+    VERDE: '#23A98C',
+    GRIS_CLARO: '#E0E0E0',
+    GRIS_MEDIO: '#95989A',
+    BACKGROUND: '#F8F9FA',
+    ROJO: '#bf0000'
+};
